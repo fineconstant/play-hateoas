1c01ae703de6dc45a105e4dd4da010f44ad2f443
Kamil Duda <kamilduda01@gmail.com>
2017-07-29 16:43:26 +0200

    59779d93d984f047aa02bd92 Sample POST

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -4,32 +4,45 @@ import java.util.UUID
 import javax.inject.{Inject, Singleton}
 
 import models.RestResource
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{JsError, JsValue, Json}
 import play.api.mvc._
+
+import scala.concurrent.Future
 
 @Singleton
 class ApiController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index: Action[AnyContent] = {
-    val resources = Seq(RestResource(UUID.randomUUID(), "name 1", "link 2"),
-                        RestResource(UUID.randomUUID(), "name 2", "link 2"))
-    Action {
-      Ok(Json.toJson(resources))
+    val resources = Seq(RestResource(UUID.randomUUID().toString, "name 1", "link 2"),
+                        RestResource(UUID.randomUUID().toString, "name 2", "link 2"))
+    Action.async {
+      val json = Json.toJson(resources)
+      Future.successful(Ok(json))
     }
   }
 
-  def show(id: String) = ???
+  def show(id: String): Action[AnyContent] = {
+    Action.async {
+      implicit request =>
+        val json = Json.toJson(RestResource(UUID.randomUUID().toString, "name x", "link x"))
+        Future.successful(Ok(json))
+    }
+  }
 
-  def process = ???
+  def process: Action[JsValue] = Action(parse.json) { implicit request =>
+    request.body.validate[RestResource]
+      .fold(
+        errors =>
+          BadRequest(Json.obj("status" -> "KO", "message" -> JsError.toJson(errors))),
+        restResource => {
+          println(restResource)
+          Ok(Json.toJson(restResource))
+        }
+      )
+  }
 
   def delete(id: String) = ???
 
   def update(id: String) = ???
 
-  private def parseJson(json: JsValue) = Action { implicit request =>
-    //val person = json.as[Person]
-    //Logger.trace(s"Parsed: $person")
-    //Ok(person)
-    ???
-  }
 }
