e88706e170a8a9958e92d5ca38bdb2d1edc7b940
Kamil Duda <kamilduda01@gmail.com>
2017-07-29 15:42:28 +0200

    59779e37fd15c6419f9cb754 Case class to Json - conversion

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -1,17 +1,20 @@
 package controllers
 
+import java.util.UUID
 import javax.inject.{Inject, Singleton}
 
-import play.api.libs.json.JsValue
+import models.RestResource
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
 
 @Singleton
 class ApiController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index: Action[AnyContent] = {
-    Action { implicit request =>
-      val r: Result = Ok("hello world")
-      r
+    val resources = Seq(RestResource(UUID.randomUUID(), "name 1", "link 2"),
+                        RestResource(UUID.randomUUID(), "name 2", "link 2"))
+    Action {
+      Ok(Json.toJson(resources))
     }
   }
 
--- app/models/RestResource.scala
+++ app/models/RestResource.scala
@@ -1,3 +1,12 @@
 package models
 
-case class RestResource(id: String, name: String, link: String)
+import java.util.UUID
+
+import play.api.libs.json.{Json, OWrites, Reads}
+
+case class RestResource(id: UUID, name: String, link: String)
+
+object RestResource {
+  implicit val restResourceWrites: OWrites[RestResource] = Json.writes[RestResource]
+  implicit val restResourceReads: Reads[RestResource] = Json.reads[RestResource]
+}
