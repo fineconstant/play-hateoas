bca7be6484d4790c4bbe1a10c4c6f86a918d16a3
Kamil Duda <kamilduda01@gmail.com>
2017-08-16 20:42:50 +0200

    Fix conversions

--- app/conversions/SlickConversions.scala
+++ app/conversions/SlickConversions.scala
@@ -5,7 +5,7 @@ import java.time.LocalDate
 
 import play.Logger
 import slick.ast.BaseTypedType
-import slick.jdbc.H2Profile.api._
+import slick.jdbc.PostgresProfile.api._
 import slick.jdbc.JdbcType
 
 object SlickConversions {
@@ -16,13 +16,15 @@ object SlickConversions {
   implicit val localDateMapper: JdbcType[LocalDate] with BaseTypedType[LocalDate] =
     MappedColumnType.base[LocalDate, Timestamp](
       (localDate: LocalDate) => {
-        val result = Timestamp valueOf localDate.toString
-        Logger.debug(s"Converting [$localDate] from LocalDate into Timestamp: [$result]")
+        Logger.debug(s"Converting from LocalDate: [$localDate]")
+        val result = Timestamp valueOf localDate.atStartOfDay
+        Logger.debug(s"LocalDate converted to Timestamp: [$result]")
         result
       },
       (timestamp: Timestamp) => {
+        Logger.debug(s"Converting from Timestamp: [$timestamp]")
         val result = timestamp.toLocalDateTime.toLocalDate
-        Logger.debug(s"Converting [$timestamp] from Timestamp into LocalDate: [$result]")
+        Logger.debug(s"Timestamp converted to LocalDate: [$result]")
         result
       }
     )
