a6915e5de161e8d616b18deaffebecc7a6dfe230
Kamil Duda <kamilduda01@gmail.com>
2017-08-17 20:26:05 +0200

    Add type

--- app/utils/UUIDPathBindable.scala
+++ app/utils/UUIDPathBindable.scala
@@ -6,7 +6,7 @@ import play.api.mvc.PathBindable
 
 // TODO: use [[utils.UUIDPathBindable]] instead of a implicit conversion in [[routers.ApiRouter]]
 object UUIDPathBindable {
-  implicit def bindable = new PathBindable[UUID] {
+  implicit def bindable: PathBindable[UUID] = new PathBindable[UUID] {
     override def bind(key: String, value: String): Either[String, UUID] = Right(UUID.fromString(value))
 
     override def unbind(key: String, id: UUID): String = id.toString
