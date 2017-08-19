ca7ff03a608e30b9aadf7f365089b5a1fe0ceef0
Kamil Duda <kamilduda01@gmail.com>
2017-08-19 20:30:10 +0200

    Add Docker script info

--- docker/start-postgres.sh
+++ docker/start-postgres.sh
@@ -8,8 +8,10 @@ DB_PASSWORD=${APP_NAME}
 CONTAINER_ID="$(docker ps -a -q -f name=${CONTAINER_NAME})"
 
 if [ -n "$CONTAINER_ID" ]; then
+    echo "Starting container [name: ${CONTAINER_NAME}, id: $CONTAINER_ID]"
     docker start ${CONTAINER_ID}
     else
+    echo "Creating a new container [${CONTAINER_NAME}]"
     docker run --name ${CONTAINER_NAME} \
             -e POSTGRES_USER=${DB_USER} \
             -e POSTGRES_PASSWORD=${DB_PASSWORD} \
