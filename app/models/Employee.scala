c51a60538b30eb7a2aac73c62b3f8e2477f47cd3
Kamil Duda <kamilduda01@gmail.com>
2017-08-19 14:32:49 +0200

    Use companion object's conversions

--- app/models/Employee.scala
+++ app/models/Employee.scala
@@ -3,9 +3,17 @@ package models
 import java.time.LocalDate
 import java.util.UUID
 
+import play.api.libs.json.{Json, OFormat, OWrites, Reads}
+
 case class Employee(
   id: UUID = UUID.randomUUID,
   firstName: String,
   lastName: String,
   birthDate: LocalDate,
   companyId: UUID)
+
+object Employee {
+  implicit val employeeWrites: OWrites[Employee] = Json.writes[Employee]
+  implicit val employeeReads: Reads[Employee] = Json.reads[Employee]
+  implicit val employeeFormat: OFormat[Employee] = Json.format[Employee]
+}
