99b048ca85825e34f4d7ae0874fb27e5d0b31a57
kduda <kamilduda01@gmail.com>
2017-11-18 21:49:56 +0100

    Disable custom db initializer

--- app/database/init/DatabaseInitializer.scala
+++ app/database/init/DatabaseInitializer.scala
@@ -1,11 +1,14 @@
 package database.init
 
 import javax.inject.Inject
-
+import play.api.Logger
 import lifecycle.Initializable
 
 final class DatabaseInitializer @Inject()(ops: InitialDatabaseOperations) extends Initializable {
-  ops.dropAllTablesIfExist()
-  ops.createSchemaIfNotExists()
-  ops.initializeWithSampleData()
+  val logger = Logger(this.getClass)
+  logger.info("Initializing database")
+
+  //  ops.dropAllTablesIfExist()
+  //  ops.createSchemaIfNotExists()
+  //  ops.initializeWithSampleData()
 }
