e78c1f1cf0321350f07ccaa462f982edaa5d0fbe
Kamil Duda <kamilduda01@gmail.com>
2017-12-03 20:51:25 +0100

    Remove more warnings

--- app/database/config/DatabaseProvider.scala
+++ app/database/config/DatabaseProvider.scala
@@ -7,7 +7,7 @@ import play.db.NamedDatabase
 import slick.jdbc.{H2Profile, PostgresProfile}
 
 /**
-  * Change db by selecting a desired NamedDatabase and getting a proper [[slick.jdbc.JdbcProfile]]
+  * Change db by selecting a desired NamedDatabase and getting a proper slick.jdbc.JdbcProfile
   * eg. h2" with [[H2Profile]] or "postgres" with [[PostgresProfile]]
   */
 @Singleton
--- app/lifecycle/Initializable.scala
+++ app/lifecycle/Initializable.scala
@@ -1,6 +1,6 @@
 package lifecycle
 
 /**
-  * Used for binding in Guice's [[com.google.inject.AbstractModule]] object implementation
+  * Used for binding in Guice's com.google.inject.AbstractModule object implementation
   */
 trait Initializable
