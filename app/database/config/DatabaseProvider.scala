e40fba0bf5cec7f9432ed1568a31afc261f2c89e
Kamil Duda <kamilduda01@gmail.com>
2017-12-04 18:34:33 +0100

    Remove more warnings

--- app/database/config/DatabaseProvider.scala
+++ app/database/config/DatabaseProvider.scala
@@ -4,11 +4,11 @@ import javax.inject.{Inject, Singleton}
 
 import play.api.db.slick.DatabaseConfigProvider
 import play.db.NamedDatabase
-import slick.jdbc.{H2Profile, PostgresProfile}
+import slick.jdbc.PostgresProfile
 
 /**
   * Change db by selecting a desired NamedDatabase and getting a proper slick.jdbc.JdbcProfile
-  * eg. h2" with [[H2Profile]] or "postgres" with [[PostgresProfile]]
+  * eg. "h2" with H2Profile or "postgres" with PostgresProfile
   */
 @Singleton
 final class DatabaseProvider @Inject()(@NamedDatabase("postgres") configProvider: DatabaseConfigProvider) {
