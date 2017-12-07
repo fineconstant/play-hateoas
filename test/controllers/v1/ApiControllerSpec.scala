68ed6fa1dd6e62368b5635b4515b59bb50687345
Kamil Duda <kamilduda01@gmail.com>
2017-12-07 18:45:15 +0100

    Add sample controller test

--- test/controllers/v1/ApiControllerSpec.scala
+++ test/controllers/v1/ApiControllerSpec.scala
@@ -1,12 +1,12 @@
 package controllers.v1
 
-import base.DefaultFlatSpec
+import base.BaseFlatSpec
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
 
-class ApiControllerSpec extends DefaultFlatSpec {
+class ApiControllerSpec extends BaseFlatSpec {
   implicit val ec: ExecutionContextExecutor = ExecutionContext.global
 
   "stream" should "be OK" in {
