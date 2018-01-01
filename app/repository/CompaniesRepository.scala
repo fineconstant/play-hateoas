544d0cbc7daaba15c02a433e14a6c26f977b35e7
Kamil Duda <kamilduda01@gmail.com>
2018-01-01 16:39:32 +0100

    Rename method

--- app/repository/CompaniesRepository.scala
+++ app/repository/CompaniesRepository.scala
@@ -13,8 +13,10 @@ import scala.concurrent.Future
 @Singleton
 class CompaniesRepository @Inject()(dbao: CompaniesDBAO) {
 
-  def companies: Source[Company, NotUsed] = dbao.stream
+  def listAll: Source[Company, NotUsed] = dbao.stream
 
   def findById(id: UUID): Future[Option[Company]] = dbao.findById(id)
+
+  def deleteById(id: UUID): Future[Int] = dbao.delete(id)
 
 }
