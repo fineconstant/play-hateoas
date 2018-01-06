4a0890b5b26ae5cef0cf74952c111ce610dcc0de
Kamil Duda <kamilduda01@gmail.com>
2018-01-06 20:36:55 +0100

    Use slick session for DDLOperations

--- app/common/db/DDLOperations.scala
+++ app/common/db/DDLOperations.scala
@@ -1,6 +1,6 @@
 package common.db
 
-import database.config.DatabaseProvider
+import akka.stream.alpakka.slick.scaladsl.SlickSession
 import database.context.DatabaseExecutionContext
 import play.Logger
 import slick.jdbc.meta.MTable
@@ -12,49 +12,48 @@ import scala.util.{Failure, Success}
   * Slick DDL operations examples
   */
 trait DDLOperations {
+  def createSchemaIfNotExists(tableName: String, schemaCreateAction: Future[Unit])
+    (implicit session: SlickSession, ec: DatabaseExecutionContext): Future[Unit] = {
 
-  def createSchemaIfNotExists(tableName: String, schemaCreateAction: Future[Unit], dbConfigProvider: DatabaseProvider)
-    (implicit ec: DatabaseExecutionContext): Future[Unit] = {
-
-    import dbConfigProvider.dbConfig._
+    import session._
 
     Logger.info(s"Checking if table [$tableName] exists")
 
     db.run(MTable.getTables)
-      .flatMap(tables => {
-        if (!tableExistsIn(tableName, tables))
-          schemaCreateAction
-            .andThen {
-              case Success(_) => Logger.info(s"Schema for table [$tableName] created")
-              case Failure(f) => Logger.info(s"Could not create schema for table [$tableName]", f)
-            }
-        else {
-          Logger.info(s"Schema for table [$tableName] already exists")
+    .flatMap(tables => {
+      if (!tableExistsIn(tableName, tables))
+        schemaCreateAction
+        .andThen {
+          case Success(_) => Logger.info(s"Schema for table [$tableName] created")
+          case Failure(f) => Logger.info(s"Could not create schema for table [$tableName]", f)
         }
-        Future.unit
-      })
+      else {
+        Logger.info(s"Schema for table [$tableName] already exists")
+      }
+      Future.unit
+    })
   }
 
-  def dropTableIfExists(tableName: String, dropTableActions: Future[Unit], dbConfigProvider: DatabaseProvider)
-    (implicit ec: DatabaseExecutionContext): Future[Unit] = {
+  def dropTableIfExists(tableName: String, dropTableActions: Future[Unit])
+    (implicit session: SlickSession, ec: DatabaseExecutionContext): Future[Unit] = {
 
-    import dbConfigProvider.dbConfig._
+    import session._
 
     Logger.info(s"Checking if table [$tableName] exists")
 
     db.run(MTable.getTables)
-      .flatMap(tables => {
-        if (tableExistsIn(tableName, tables))
-          dropTableActions
-            .andThen {
-              case Success(_) => Logger.info(s"Table [$tableName] dropped")
-              case Failure(f) => Logger.info(s"Could not drop table [$tableName]", f)
-            }
-        else {
-          Logger.info(s"Table [$tableName] does not exist")
+    .flatMap(tables => {
+      if (tableExistsIn(tableName, tables))
+        dropTableActions
+        .andThen {
+          case Success(_) => Logger.info(s"Table [$tableName] dropped")
+          case Failure(f) => Logger.info(s"Could not drop table [$tableName]", f)
         }
-        Future.unit
-      })
+      else {
+        Logger.info(s"Table [$tableName] does not exist")
+      }
+      Future.unit
+    })
   }
 
   private def tableExistsIn(table: String, dbTables: Vector[MTable]): Boolean =
