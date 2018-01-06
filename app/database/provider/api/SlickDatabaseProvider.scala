b19f2d7bd19ffbc36442802100c9f55e289dff0d
Kamil Duda <kamilduda01@gmail.com>
2018-01-06 21:10:08 +0100

    Create DB Provider hierarchy

---
+++ app/database/provider/api/SlickDatabaseProvider.scala
@@ -0,0 +1,9 @@
+package database.provider.api
+
+import akka.stream.alpakka.slick.javadsl.SlickSession
+
+trait SlickDatabaseProvider {
+  protected val sessionProvider: SlickSessionProvider
+  val session: SlickSession = sessionProvider.session
+  val db = session.db
+}
